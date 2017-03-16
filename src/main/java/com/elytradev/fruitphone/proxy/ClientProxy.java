a174b538f1635a502cdab499fdc0d04284568e12
Aesen Vismea <aesen@unascribed.com>
2017-03-15 21:11:21 -0400

    Don't show the spinner on servers lacking Fruit Phone. Fixes #9

--- src/main/java/com/elytradev/fruitphone/proxy/ClientProxy.java
+++ src/main/java/com/elytradev/fruitphone/proxy/ClientProxy.java
@@ -79,7 +79,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class ClientProxy extends Proxy {
-	private boolean isServerVanilla = false;
+	private static boolean isServerVanilla = false;
 	public boolean alwaysOn = false;
 	
 	public static float ticks;
@@ -189,15 +189,13 @@ public class ClientProxy extends Proxy {
 			// check if any of the following are true:
 			// 1. optional mode is enabled
 			// 2. the fruitphone:alwaysOn gamerule is set to true
-			// 3. we are connected to a Forge server without Fruit Phone
-			// 4. we are connected to a vanilla server
-			// 5. the player has equipped glasses
+			// 3. we are connected to a server without Fruit Phone
+			// 4. the player has equipped glasses
 			ItemStack glasses = null;
 			if (
 				FruitPhone.inst.optionalMode ||
 				alwaysOn ||
-				Item.REGISTRY.getNameForObject(FruitItems.PASSIVE) == null ||
-				isServerVanilla ||
+				!doesServerHaveMod() ||
 				(
 					Minecraft.getMinecraft().player.hasCapability(FruitPhone.CAPABILITY_EQUIPMENT, null) &&
 					!(glasses = Minecraft.getMinecraft().player.getCapability(FruitPhone.CAPABILITY_EQUIPMENT, null).glasses).isEmpty()
@@ -252,6 +250,11 @@ public class ClientProxy extends Proxy {
 				} GlStateManager.popMatrix();
 			}
 		}
+	}
+
+	public static boolean doesServerHaveMod() {
+		return Item.REGISTRY.getNameForObject(FruitItems.PASSIVE) != null &&
+				!isServerVanilla;
 	}
 
 	@SubscribeEvent
