d9dc6e00f860e5741bac5dc4dd3e6667cd3ab465
Aesen Vismea <aesen@unascribed.com>
2017-12-03 04:11:03 -0500

    Update to 1.12.2

--- src/main/java/com/elytradev/fruitphone/compat/jei/FruitPhoneJEIPlugin.java
+++ src/main/java/com/elytradev/fruitphone/compat/jei/FruitPhoneJEIPlugin.java
@@ -27,19 +27,19 @@ package com.elytradev.fruitphone.compat.jei;
 import com.elytradev.fruitphone.item.FruitItems;
 import com.elytradev.fruitphone.item.ItemFruit;
 
-import mezz.jei.api.BlankModPlugin;
+import mezz.jei.api.IModPlugin;
 import mezz.jei.api.ISubtypeRegistry;
 import mezz.jei.api.JEIPlugin;
 import mezz.jei.api.ISubtypeRegistry.ISubtypeInterpreter;
 import net.minecraft.item.ItemStack;
 
 @JEIPlugin
-public class FruitPhoneJEIPlugin extends BlankModPlugin {
+public class FruitPhoneJEIPlugin implements IModPlugin {
 	@Override
 	public void registerItemSubtypes(ISubtypeRegistry subtypeRegistry) {
 		ISubtypeInterpreter interpreter = new ISubtypeInterpreter() {
 			@Override
-			public String getSubtypeInfo(ItemStack itemStack) {
+			public String apply(ItemStack itemStack) {
 				return itemStack.getItem().getRegistryName()+"~"+itemStack.getMetadata()+"~"+((ItemFruit)itemStack.getItem()).getColor(itemStack);
 			}
 		};
